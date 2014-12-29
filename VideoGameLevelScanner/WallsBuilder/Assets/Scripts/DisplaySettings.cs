44e6e8dc0c8f3e4cd2da3b4a62e62592882efd8d
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2014-12-29 21:52:22 +0100

    Poprawka błędnie skonfigurowanych properisów kierunków.

--- VideoGameLevelScanner/WallsBuilder/Assets/Scripts/DisplaySettings.cs
+++ VideoGameLevelScanner/WallsBuilder/Assets/Scripts/DisplaySettings.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class DisplaySettings : MonoBehaviour {
+public class MultiWallScript : MonoBehaviour {
 
     public enum Mode { Half, Full, Empty }
 	public enum Side { Top, Bottom, Left, Right }
@@ -17,15 +17,18 @@ public class DisplaySettings : MonoBehaviour {
 	[SerializeField]
 	private Mode top = Mode.Empty;
 	public Mode Top { get { return top; } set { top = value; ChangeVisibility(Side.Top,value); } }
-	[SerializeField]
+	
+    [SerializeField]
 	private Mode bottom = Mode.Empty;
-	public Mode Right { get { return bottom; } set { bottom = value; } }
-	[SerializeField]
+	public Mode Bottom { get { return bottom; } set { bottom = value; ChangeVisibility(Side.Bottom,value); } }
+	
+    [SerializeField]
 	private Mode left = Mode.Empty;
-	public Mode Bottom { get { return left; } set { left = value; } }
-	[SerializeField]
+	public Mode Left { get { return left; } set { left = value; ChangeVisibility(Side.Left,value); } }
+	
+    [SerializeField]
 	private Mode right = Mode.Empty;
-	public Mode Left { get { return right; } set { right = value; } }
+	public Mode Right { get { return right; } set { right = value; ChangeVisibility(Side.Right,value); } }
 
 	void Start () {
 		modesMeshes= new Dictionary<Mode, Mesh>(){ {Mode.Empty,emptyMesh} , {Mode.Half,halfMesh}, {Mode.Full,fullMesh}};
@@ -35,13 +38,11 @@ public class DisplaySettings : MonoBehaviour {
     {
 		MeshFilter meshFilter = this.transform.FindChild(SidesNames [(int)side]).GetComponent<MeshFilter>();
 		meshFilter.mesh = modesMeshes [mode];
-      
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-			Top = Mode.Full;
+		
 	}
 
 }
