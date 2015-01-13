45ac152129820644b82c8077bbbe933460756903
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2015-01-13 03:46:45 +0100

    Proof of concept na potrzeby dema na seminarium dyplomowe.

--- VideoGameLevelScanner/WallsBuilder/Assets/Scripts/IntegrationTest.cs
+++ VideoGameLevelScanner/WallsBuilder/Assets/Scripts/IntegrationTest.cs
@@ -21,6 +21,8 @@ public class IntegrationTest : MonoBehaviour {
     private Board board;
     private Timer cameraTimer;
     private FilteringParameters filteringParameters;
+    public Hsv EditedRangeMax;
+    public Hsv EditedRangeMin;
 	private Texture2D cameraTex;
     private Texture2D lookupTex;
 
@@ -32,6 +34,9 @@ public class IntegrationTest : MonoBehaviour {
         cameraTimer = new Timer(80);
         cameraTimer.Elapsed += new ElapsedEventHandler(OnCameraFrame);
         capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_FPS, 25);
+        EditedRangeMax = new Hsv();
+        EditedRangeMin = new Hsv();
+
 	}
 	private void Start()
 	{
@@ -58,12 +63,12 @@ public class IntegrationTest : MonoBehaviour {
 
     private void AnalyseFrame()
     {
-        
-
         frame = capture.QueryFrame();
         if (frame != null) { 
             cameraTex = TextureConvert.ImageToTexture2D<Bgr, byte>(frame, true);
+            Sprite.DestroyImmediate(GameObject.Find("CameraImage").GetComponent<UnityEngine.UI.Image>().sprite);
             GameObject.Find("CameraImage").GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(cameraTex, new Rect(0, 0, cameraTex.width, cameraTex.height), new Vector2(0.5f, 0.5f));
+
         }
         if (!processingFrame) {
             Debug.Log("Analysis fired");
@@ -79,6 +84,7 @@ public class IntegrationTest : MonoBehaviour {
             if (lookupImage != null)
             {
                 lookupTex = TextureConvert.ImageToTexture2D<Bgr, byte>(lookupImage, true);
+                Sprite.DestroyImmediate(GameObject.Find("Lookup").GetComponent<UnityEngine.UI.Image>().sprite);
                 GameObject.Find("Lookup").GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(lookupTex, new Rect(0, 0, lookupTex.width, lookupTex.height), new Vector2(0.5f, 0.5f));
             }
             processingFrame = false;
@@ -105,6 +111,51 @@ public class IntegrationTest : MonoBehaviour {
     //    }
     //}
 
+    public float EditedHueMax { get { return (float)this.EditedRangeMax.Hue; } set { EditedRangeMax.Hue = value; } }
+    public float EditedSaturationMax { get { return (float)this.EditedRangeMax.Satuation; } set { EditedRangeMax.Satuation = value; } }
+    public float EditedValueMax { get { return (float)this.EditedRangeMax.Value; } set { EditedRangeMax.Value = value; } }
+    public float EditedHueMin { get { return (float)this.EditedRangeMin.Hue; } set { EditedRangeMin.Hue = value; } }
+    public float EditedSaturationMin { get { return (float)this.EditedRangeMin.Satuation; } set { EditedRangeMin.Satuation = value; } }
+    public float EditedValueMin { get { return (float)this.EditedRangeMin.Value; } set { EditedRangeMin.Value = value; } }
+    
+    public void GoToBuilding()
+    {
+        cameraTimer.Stop();
+        capture.Stop();
+        this.gameObject.SetActive(false);
+        GameObject.Find("LevelCreator").GetComponent<LevelBuilder>().BuildLevel(board);
+    }
+
+    public enum EditedRange {Blue=0,Red=1, Red2=2, Green=3, Yellow=4}
+    public void ChangeEditedRange(int range)
+    {
+        EditedRange selectedColor = (EditedRange)range;
+        switch (selectedColor)
+        {
+            case EditedRange.Blue:
+                EditedRangeMax = filteringParameters.BlueMax;
+                EditedRangeMin = filteringParameters.BlueMin;
+                break;
+            case EditedRange.Red:
+                EditedRangeMax = filteringParameters.RedMax;
+                EditedRangeMin = filteringParameters.RedMin;
+                break;
+            case EditedRange.Red2:
+                EditedRangeMax = filteringParameters.RedMax2;
+                EditedRangeMin = filteringParameters.RedMin2;
+                break;
+            case EditedRange.Green:
+                EditedRangeMax = filteringParameters.GreenMax;
+                EditedRangeMin = filteringParameters.GreenMin;
+                break;
+            case EditedRange.Yellow:
+                EditedRangeMax = filteringParameters.YellowMax;
+                EditedRangeMin = filteringParameters.YellowMin;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
     private void OnDestroy()
     {
         cameraTimer.Stop();
