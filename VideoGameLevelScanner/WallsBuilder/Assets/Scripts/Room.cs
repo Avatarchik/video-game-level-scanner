969c8fa4d6e574394053ed72ca4a2b1b1cb321d3
Anna Witwicka <witwickaa@student.mini.pw.edu.pl>
2015-01-23 00:06:47 +0100

    Kolorowanie podłóg, refactoring level builder
    Related Work Items: #174

--- VideoGameLevelScanner/WallsBuilder/Assets/Scripts/Room.cs
+++ VideoGameLevelScanner/WallsBuilder/Assets/Scripts/Room.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-public class Room {
+public class Room{
     public List<Floor> floors = new List<Floor>();
     public List<Wall> Walls = new List<Wall>(); 
     public int N;
     public Material WallMaterial;
     public Material FloorMaterial;
 
-    public Room(int n)
+    public Room(int n, Material floorMaterial)
     {
         N = n;
+        FloorMaterial = new Material(floorMaterial);
+        FloorMaterial.name = "floorMat";
+        FloorMaterial.color = Color.gray;
     }
     public override string ToString()
     {
@@ -21,7 +24,23 @@ public class Room {
             sb.Append("(" + floor.X + "," + floor.Y + ") ");
         return "Room" + N + ": " + sb.ToString();
     }
-    public void SetRoomMaterial() {
+    public void SetWallMaterial() 
+    {
         
+    }
+    public void SetRoomMaterial()
+    {
+        foreach (var floor in floors)
+        {
+            floor.gameObject.renderer.material = FloorMaterial;
+        } 
+    }
+    public void ChangeFloorColor(Color color)
+    {
+        FloorMaterial.color = color;
+    }
+    public void ChangeWallMaterial()
+    {
+
     }
 }
