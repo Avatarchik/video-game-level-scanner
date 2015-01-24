2615ceba63a8cc0cd9640558e3ff7730737efacc
Anna Witwicka <witwickaa@student.mini.pw.edu.pl>
2015-01-25 00:31:59 +0100

    ExitVisitMode script
    Related Work Items: #182, #185

---
+++ VideoGameLevelScanner/WallsBuilder/Assets/Scripts/ExitVisitMode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitVisitMode : MonoBehaviour {
+    public LevelBuilder Level;
+    public GameObject VisitModeUI;
+    public GameObject BuildModeUI;
+    public GameObject Player;
+    public Camera PlayerCamera;
+    public Camera MainCamera;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Player.SetActive(false);
+            PlayerCamera.enabled = false;
+            Level.transform.localScale = new Vector3(1f,1f,1f);
+            MainCamera.enabled = true;
+            BuildModeUI.SetActive(true);
+            VisitModeUI.SetActive(false);
+        }
+	}
+}
